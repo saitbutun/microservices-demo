b3895be0ff1da481e5eae987d9bb8bf99874abf6
saitbutun <sait.butun@kloia.com>
2026-01-19 09:29:07 +0300

    Update ecr.tf

--- .github/terraform/ecr.tf
+++ .github/terraform/ecr.tf
@@ -1,8 +1,8 @@
 resource "aws_ecr_repository" "microservices" {
   for_each             = var.services
-  name                 = "${var.app_name}/${each.key}" # Örn: microservices-demo/frontend
+  name                 = "${var.app_name}/${each.key}" 
   image_tag_mutability = "MUTABLE"
-  force_delete         = true # Demo olduğu için, içinde imaj olsa bile silmeye izin ver
+  force_delete         = true 
 
   image_scanning_configuration {
     scan_on_push = true
